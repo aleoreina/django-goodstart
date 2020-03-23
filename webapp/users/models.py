7027c48bfa279d3d609654f6693f55c62a89b45d
Angel <aleoreina@gmail.com>
2020-03-23 16:13:21 -0400

    create_superuser() got an unexpected argument 'username' Fixed + Others fixes on manager

--- webapp/users/models.py
+++ webapp/users/models.py
@@ -4,17 +4,26 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import models as auth_models
 
 class UserManager(auth_models.BaseUserManager):
-
-    def create_user(self, email, password=None):
+  
+    def create_user(self, username, first_name, 
+                        last_name, dob, email, password):
         if not email:
             raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email))
+        user.first_name = first_name
+        user.last_name = last_name
+        user.dob = dob
         user.set_password(password)
         user.save(using=self._db)
         return user
+    
+    def create_superuser(self, username, first_name, 
+                        last_name, dob, email, password ) :
 
-    def create_superuser(self, email, password):
-        user = self.create_user(email, password=password)
+        user = self.create_user(username=username, first_name=first_name, 
+                                last_name=last_name, dob=dob, 
+                                email=email, password=password)
+
         user.is_superuser = user.is_staff = True
         user.save(using=self._db)
         return user
@@ -35,6 +44,8 @@ class User(AbstractUser):
 	)
 
     objects = UserManager()
+
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'email', 'dob']
 
     class Meta:
         verbose_name = 'Organizer / Participant'
