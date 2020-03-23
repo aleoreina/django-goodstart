07f6c75f5c3830e4ea15e459dfb81453c57ddf59
Angel <aleoreina@gmail.com>
2020-03-22 23:19:23 -0400

    Static folders added and Static settings added..

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -125,4 +125,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 AUTH_USER_MODEL = 'users.User'
