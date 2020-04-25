30dda6ebda527dc6a7d7bb0df8c5da78acde3d19
Angel <aleoreina@gmail.com>
2020-04-25 19:16:28 -0400

    Added potential protection for Secret Keys ! (idea by @aleoreina)

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -23,7 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gp-4wcb&(kyxwvcgqzmn(soylzle1pptaaz!b2wx4r4j%on537'
+def SecretKeyGenerator(size=128, chars=string.ascii_letters + string.digits):
+    return ''.join(random.choice(chars) for i in range(size))
+
+SECRET_KEY = SecretKeyGenerator()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
