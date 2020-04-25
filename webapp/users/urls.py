c5547cb893708e09c13170269b946421f4dc2d02
Angel <aleoreina@gmail.com>
2020-04-25 19:13:09 -0400

    Register disabled temp.

--- webapp/users/urls.py
+++ webapp/users/urls.py
@@ -5,5 +5,5 @@ from .views import LoginView, RegisterView
 app_name = 'users'
 urlpatterns = [
     path('login', LoginView.as_view(), name="users-login"),
-    path('register', RegisterView.as_view(), name="users-register"),
+   # path('register', RegisterView.as_view(), name="users-register"),
 ]
